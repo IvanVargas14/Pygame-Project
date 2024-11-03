5c3f21b67d052615cc395acefb556716fd9028eb
IvanVargas14 <ivan.vargas02@outlook.com>
2024-11-03 16:56:43 -0500

    Update Assignment 4A.py

--- Assignment 4A.py
+++ Assignment 4A.py
@@ -11,45 +11,60 @@ from pygame.locals import *
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 
-# defining the paddle
-paddle = pygame.Surface((100, 20))
-paddle.fill((255, 255, 255))
-paddle_rect = pygame.Rect(350, 580, 100, 20)
-
-# defining the ball
-ball = pygame.Surface((20, 20))
-ball.fill((255, 0, 0))
-ball_rect = pygame.Rect(200, 200, 20, 20)
-
-ball2 = pygame.Surface((20, 20))
-#ball2.fill((0, 255, 0))
-ball2_rect = pygame.Rect(400, 200, 20, 20)
-#ball_rect.x = random.randint(0, 800 - 20)
-
-# variables
-speed = 3
-speedB = 10
-point = 0
-level = 1 #initialize level
-list_of_balls = []
-clock = pygame.time.Clock()
-goingRight = True
-goingDown = True
-goingRight2 = True
-goingDown2 = True
-font = pygame.font.SysFont(None, 36) #Use default system font, size 36
-# while running statement, begins with quit
 
 def generate_random_color():
     r = random.randint(0,255)
     g = random.randint(0,255)
     b = random.randint(0,255)
     return (r, g, b)
+
 random_color = generate_random_color()
 
 
+# defining the paddle
+paddle = pygame.Surface((100, 20))
+paddle.fill((255, 255, 255))
+paddle_rect = pygame.Rect(350, 580, 100, 20)
 
 
+# defining the ball
+ball = pygame.Surface((20, 20))
+ball.fill((random_color))
+ball_rect = pygame.Rect(200, 200, 20, 20)
+
+
+ball2 = pygame.Surface((20, 20))
+#ball2.fill((0, 255, 0))
+ball2_rect = pygame.Rect(400, 200, 20, 20)
+#ball_rect.x = random.randint(0, 800 - 20)
+
+
+# variables
+speed = 3
+speedB = 10
+point = 0
+level = 1 #initialize level
+
+
+# time variables
+survival_time = 0
+currentime = pygame.time.get_ticks()
+clock = pygame.time.Clock()
+
+
+#lists
+list_of_balls = []
+
+
+# going signs
+goingRight = True
+goingDown = True
+goingRight2 = True
+goingDown2 = True
+font = pygame.font.SysFont(None, 36) #Use default system font, size 36
+
+
+# while running statement, begins with quit
 while True:
     keys = pygame.key.get_pressed()
     for event in pygame.event.get():
@@ -98,12 +113,12 @@ while True:
     if ball_rect.y == 580:
         print("Score:", point)
         sys.exit(0)
-    elif point >=3:
+    elif point >=1:
 
             level = 2
-            speed = 8
+            speed = 5
             ball2 = pygame.Surface((20, 20))
-            ball2.fill((random_color)) # most important line to have this visible?
+            ball2.fill((255,0,0))  # most important line to have this visible?
             pygame.display.flip()
             clock.tick(60)
             if goingDown2:
@@ -120,7 +135,7 @@ while True:
 
             # sends ball up if rect touches very bottom of screen
             if ball2_rect.y > 580:
-                ball2_rect.x = random.randint(0, 800 - 20)
+                ball_2rect.x = random.randint(0, 800 - 20)
                 ball2_rect.y = 10
                 point -= 1
 
@@ -138,17 +153,24 @@ while True:
     elif point == 5:
         level = 3
         list_of_balls.clear()
-    elif point == 8:
+    elif point == 20:
         level = 4
         speed = 7
-    elif point == 10:
+    elif point == 35:
         level = 5
     elif point == 50:
         level = 6
         speed = 9
     elif point == 60:
         level = 7
-
+    elif point == 75:
+        level = 8
+        speed = 11
+    elif point == 90:
+        level = 9
+    elif point == 100:
+        level = 10
+        speed = 13
     # Render the score and level as text
     score_text = font.render(f'Score: {point}', True, (255,255,255))
     level_text = font.render(f'Level: {level}', True, (255,255,255))
