1488961be5c2dd40c0ba2b6be6d2fac6cf1d3427
IvanVargas14 <ivan.vargas02@outlook.com>
2024-11-05 22:15:20 -0500

    Update Assignment 4A.py

--- Assignment 4A.py
+++ Assignment 4A.py
@@ -11,23 +11,7 @@ from pygame.locals import *
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 resolution = (800,600)
-#
-# def spawn_ball():
-#     size = 20
-#     side = (["top"])
-#     speed2 = speed + (currentime + 5000)
-#
-#     if side == "top":
-#         ball_rect = pygame.Rect(random.randint(0,800-size),0 , size , size )
-#         dx = 0
-#         dy = speed3
-#     list_of_balls.append((ball_rect, dx, dy))
-#
-#
-# def update_screen():
-#     for ball, _, _ in list_of_balls:
-#         pygame.draw.rect(screen, color(random_color), meteor)
-#
+
 
 def generate_random_color():
     r = random.randint(0,255)
@@ -52,12 +36,16 @@ ball_rect = pygame.Rect(200, 200, 20, 20)
 
 ball2 = pygame.Surface((20, 20))
 #ball2.fill((0, 255, 0))
-ball2_rect = pygame.Rect(400, 0, 20, 20)
+ball2_rect = pygame.Rect(350, 0, 20, 20)
 #ball_rect.x = random.randint(0, 800 - 20)
 
+ball3 = pygame.Surface((20, 20))
+ball3_rect = pygame.Rect(500, 0, 20, 20)
 
 # variables
 speed = 4
+speed2 = 4
+speed3= 5
 speedB = 10
 point = 0
 level = 1 #initialize level
@@ -82,6 +70,9 @@ goingRight = True
 goingDown = True
 goingRight2 = True
 goingDown2 = True
+goingRight3 = True
+goingDown3 = True
+
 font = pygame.font.SysFont(None, 36) #Use default system font, size 36
 
 
@@ -137,10 +128,19 @@ while True:
     if ball_rect.y == 580:
         print("Score:", point)
         sys.exit(0)
+
+    if ball2_rect.y == 580:
+        print("Score:", point)
+        sys.exit(0)
+    if ball3_rect.y == 580:
+        print("Score:", point)
+        sys.exit(0)
+
     elif 2 <= point <=3:
 
             level = 2
             speed = 5
+
             ball2 = pygame.Surface((20, 20))
             ball2.fill((255,0,0))  # most important line to have this visible?
             pygame.display.flip()
@@ -177,37 +177,44 @@ while True:
 
 
     elif 4 <= point <=6:
-        level = 3
-        speed = 6
+            level = 3
+            speed = 6
 
-        if goingDown2:
-            ball2_rect = ball2_rect.move(0, speed)
-        else:
-            ball2_rect = ball2_rect.move(0, -speed)
+            if goingDown2:
+                ball2_rect = ball2_rect.move(0, speed)
+            else:
+                ball2_rect = ball2_rect.move(0, -speed)
 
-        if ball2_rect.y < 0:
-            goingDown2 = True
-        # adds point and reflects ball upon collision
-        if paddle_rect.colliderect(ball2_rect):
-            point += 1
-            goingDown2 = False
+            if ball2_rect.y < 0:
+                goingDown2 = True
+            # adds point and reflects ball upon collision
+            if paddle_rect.colliderect(ball2_rect):
+                point += 1
+                goingDown2 = False
 
-        # sends ball up if rect touches very bottom of screen
-        if ball2_rect.y > 580:
-            ball2_rect.x = random.randint(0, 800 - 20)
-            ball2_rect.y = 10
-            point -= 1
+            # sends ball up if rect touches very bottom of screen
+            if ball2_rect.y > 580:
+                ball2_rect.x = random.randint(0, 800 - 20)
+                ball2_rect.y = 10
+                point -= 1
 
-        # movement of ball right and left across X-axis
-        if goingRight2:
-            ball2_rect = ball2_rect.move(speed, 0)
-        else:
-            ball2_rect = ball2_rect.move(-speed, 0)
-        # borders of right and left screen
-        if ball2_rect.x > 780:
-            goingRight2 = False
-        if ball2_rect.x < 0:
-            goingRight2 = True
+            # movement of ball right and left across X-axis
+            if goingRight2:
+                ball2_rect = ball2_rect.move(speed, 0)
+            else:
+                ball2_rect = ball2_rect.move(-speed, 0)
+            # borders of right and left screen
+            if ball2_rect.x > 780:
+                goingRight2 = False
+            if ball2_rect.x < 0:
+                goingRight2 = True
+
+            if ball_rect.y == 580:
+                print("Score:", point)
+                sys.exit(0)
+            if ball2_rect.y == 580:
+                print("Score:", point)
+                sys.exit(0)
 
         #list_of_balls.clear()
 
@@ -215,62 +222,9 @@ while True:
 
 
     elif 7 <= point <=9:
-        level = 4
-        speed = 6.1
+            level = 4
+            speed = 6
 
-
-        if goingDown2:
-            ball2_rect = ball2_rect.move(0, speed)
-        else:
-            ball2_rect = ball2_rect.move(0, -speed)
-
-        if ball2_rect.y < 0:
-            goingDown2 = True
-            # adds point and reflects ball upon collision
-        if paddle_rect.colliderect(ball2_rect):
-            point += 1
-            goingDown2 = False
-
-            # sends ball up if rect touches very bottom of screen
-        if ball2_rect.y > 580:
-            ball2_rect.x = random.randint(0, 800 - 20)
-            ball2_rect.y = 10
-            point -= 1
-
-            # movement of ball right and left across X-axis
-        if goingRight2:
-            ball2_rect = ball2_rect.move(speed, 0)
-        else:
-            ball2_rect = ball2_rect.move(-speed, 0)
-            # borders of right and left screen
-        if ball2_rect.x > 780:
-            goingRight2 = False
-        if ball2_rect.x < 0:
-            goingRight2 = True
-
-
-        if increasing:
-            color += 1
-        else:
-            color -= 1
-        if color >= 255:
-                increasing = False
-        if color <= 0:
-                increasing = True
-
-
-        elif 10 <= point <=11:
-            level = 5
-            #increasing = False
-
-            if increasing:
-                color += 1
-            else:
-                color -= 1
-            if color >= 255:
-                increasing = False
-            if color <= 0:
-                increasing = True
 
             if goingDown2:
                 ball2_rect = ball2_rect.move(0, speed)
@@ -302,12 +256,302 @@ while True:
                 goingRight2 = True
 
 
-        elif 12 <= point <13:
+            if increasing:
+                color += 1
+            else:
+                color -= 1
+            if color >= 255:
+                    increasing = False
+            if color <= 0:
+                    increasing = True
+
+            if ball_rect.y == 580:
+                print("Score:", point)
+                sys.exit(0)
+            if ball2_rect.y == 580:
+                print("Score:", point)
+                sys.exit(0)
+
+    elif 10 <= point <=12:
+            level = 5
+            speed = 6
+            speed2 = 4
+            Speed2 = 4
+
+
+            ball3 = pygame.Surface((20, 20))
+            ball3.fill((0, 0, 255))  # most important line to have this visible?
+            pygame.display.flip()
+            # clock.tick(60)
+            if goingDown3:
+                ball3_rect = ball3_rect.move(0, speed2)
+            else:
+                ball3_rect = ball3_rect.move(0, -speed2)
+
+            if ball3_rect.y < 0:
+                goingDown3 = True
+            # adds point and reflects ball upon collision
+            if paddle_rect.colliderect(ball3_rect):
+                point += 1
+                goingDown3 = False
+
+            # sends ball up if rect touches very bottom of screen
+            if ball3_rect.y > 580:
+                ball3_rect.x = random.randint(0, 800 - 20)
+                ball3_rect.y = 10
+                point -= 1
+
+            # movement of ball right and left across X-axis
+            if goingRight3:
+                ball3_rect = ball3_rect.move(speed2, 0)
+            else:
+                ball3_rect = ball3_rect.move(-speed2, 0)
+            # borders of right and left screen
+            if ball3_rect.x > 780:
+                goingRight3 = False
+            if ball3_rect.x < 0:
+                goingRight3 = True
+
+
+
+
+            if increasing:
+                color += 1
+            else:
+                color -= 1
+            if color >= 255:
+                increasing = False
+            if color <= 0:
+                increasing = True
+
+
+            if goingDown2:
+                ball2_rect = ball2_rect.move(0, speed)
+            else:
+                ball2_rect = ball2_rect.move(0, -speed)
+
+            if ball2_rect.y < 0:
+                goingDown2 = True
+                # adds point and reflects ball upon collision
+            if paddle_rect.colliderect(ball2_rect):
+                point += 1
+                goingDown2 = False
+
+                # sends ball up if rect touches very bottom of screen
+            if ball2_rect.y > 580:
+                ball2_rect.x = random.randint(0, 800 - 20)
+                ball2_rect.y = 10
+                point -= 1
+
+                # movement of ball right and left across X-axis
+            if goingRight2:
+                ball2_rect = ball2_rect.move(speed, 0)
+            else:
+                ball2_rect = ball2_rect.move(-speed, 0)
+                # borders of right and left screen
+            if ball2_rect.x > 780:
+                goingRight2 = False
+            if ball2_rect.x < 0:
+                goingRight2 = True
+
+            if ball_rect.y == 580:
+                print("Score:", point)
+                sys.exit(0)
+
+            if ball2_rect.y == 580:
+                print("Score:", point)
+                sys.exit(0)
+            if ball3_rect.y == 580:
+                print("Score:", point)
+                sys.exit(0)
+
+    elif 13 <= point <=14:
             level = 6
             speed = 8
-        elif 14 <= point <15:
-            level = 7
-            speed = 9
+#so rn, speed2 is assigned to ball three
+# so to fix this, i need to make speed3 = to 4
+# assign ball 3 to speed 3
+# and assign ball 2 to speed 2
+            speed2 = 6
+            speed3 = 4
+
+            ball3 = pygame.Surface((20, 20))
+            ball3.fill((0, 0, 255))  # most important line to have this visible?
+            pygame.display.flip()
+            # clock.tick(60)
+            if goingDown3:
+                ball3_rect = ball3_rect.move(0, speed3)
+            else:
+                ball3_rect = ball3_rect.move(0, -speed3)
+
+            if ball3_rect.y < 0:
+                goingDown3 = True
+            # adds point and reflects ball upon collision
+            if paddle_rect.colliderect(ball3_rect):
+                point += 1
+                goingDown3 = False
+
+            # sends ball up if rect touches very bottom of screen
+            if ball3_rect.y > 580:
+                ball3_rect.x = random.randint(0, 800 - 20)
+                ball3_rect.y = 10
+                point -= 1
+
+            # movement of ball right and left across X-axis
+            if goingRight3:
+                ball3_rect = ball3_rect.move(speed3, 0)
+            else:
+                ball3_rect = ball3_rect.move(-speed3, 0)
+            # borders of right and left screen
+            if ball3_rect.x > 780:
+                goingRight3 = False
+            if ball3_rect.x < 0:
+                goingRight3 = True
+
+            if increasing:
+                color += 1
+            else:
+                color -= 1
+            if color >= 255:
+                increasing = False
+            if color <= 0:
+                increasing = True
+
+            if goingDown2:
+                ball2_rect = ball2_rect.move(0, speed2)
+            else:
+                ball2_rect = ball2_rect.move(0, -speed2)
+
+            if ball2_rect.y < 0:
+                goingDown2 = True
+                # adds point and reflects ball upon collision
+            if paddle_rect.colliderect(ball2_rect):
+                point += 1
+                goingDown2 = False
+
+                # sends ball up if rect touches very bottom of screen
+            if ball2_rect.y > 580:
+                ball2_rect.x = random.randint(0, 800 - 20)
+                ball2_rect.y = 10
+                point -= 1
+
+                # movement of ball right and left across X-axis
+            if goingRight2:
+                ball2_rect = ball2_rect.move(speed2, 0)
+            else:
+                ball2_rect = ball2_rect.move(-speed2, 0)
+                # borders of right and left screen
+            if ball2_rect.x > 780:
+                goingRight2 = False
+            if ball2_rect.x < 0:
+                goingRight2 = True
+
+            if ball_rect.y == 580:
+                print("Score:", point)
+                sys.exit(0)
+
+            if ball2_rect.y == 580:
+                print("Score:", point)
+                sys.exit(0)
+            if ball3_rect.y == 580:
+                print("Score:", point)
+                sys.exit(0)
+
+    elif 15 <= point <=17:
+                level = 7
+                speed = 8
+
+                speed2 = 7
+                speed3 = 5
+
+                ball3 = pygame.Surface((20, 20))
+                ball3.fill((0, 0, 255))  # most important line to have this visible?
+                pygame.display.flip()
+                # clock.tick(60)
+                if goingDown3:
+                    ball3_rect = ball3_rect.move(0, speed3)
+                else:
+                    ball3_rect = ball3_rect.move(0, -speed3)
+
+                if ball3_rect.y < 0:
+                    goingDown3 = True
+                # adds point and reflects ball upon collision
+                if paddle_rect.colliderect(ball3_rect):
+                    point += 1
+                    goingDown3 = False
+
+                # sends ball up if rect touches very bottom of screen
+                if ball3_rect.y > 580:
+                    ball3_rect.x = random.randint(0, 800 - 20)
+                    ball3_rect.y = 10
+                    point -= 1
+
+                # movement of ball right and left across X-axis
+                if goingRight3:
+                    ball3_rect = ball3_rect.move(speed3, 0)
+                else:
+                    ball3_rect = ball3_rect.move(-speed3, 0)
+                # borders of right and left screen
+                if ball3_rect.x > 780:
+                    goingRight3 = False
+                if ball3_rect.x < 0:
+                    goingRight3 = True
+
+                if increasing:
+                    color += 1
+                else:
+                    color -= 1
+                if color >= 255:
+                    increasing = False
+                if color <= 0:
+                    increasing = True
+
+                if goingDown2:
+                    ball2_rect = ball2_rect.move(0, speed2)
+                else:
+                    ball2_rect = ball2_rect.move(0, -speed2)
+
+                if ball2_rect.y < 0:
+                    goingDown2 = True
+                    # adds point and reflects ball upon collision
+                if paddle_rect.colliderect(ball2_rect):
+                    point += 1
+                    goingDown2 = False
+
+                    # sends ball up if rect touches very bottom of screen
+                if ball2_rect.y > 580:
+                    ball2_rect.x = random.randint(0, 800 - 20)
+                    ball2_rect.y = 10
+                    point -= 1
+
+                    # movement of ball right and left across X-axis
+                if goingRight2:
+                    ball2_rect = ball2_rect.move(speed2, 0)
+                else:
+                    ball2_rect = ball2_rect.move(-speed2, 0)
+                    # borders of right and left screen
+                if ball2_rect.x > 780:
+                    goingRight2 = False
+                if ball2_rect.x < 0:
+                    goingRight2 = True
+
+                if ball_rect.y == 580:
+                    print("Score:", point)
+                    sys.exit(0)
+
+                if ball2_rect.y == 580:
+                    print("Score:", point)
+                    sys.exit(0)
+                if ball3_rect.y == 580:
+                    print("Score:", point)
+                    sys.exit(0)
+
+
+    elif point == 18:
+                    print("Score:", point)
+                    sys.exit(0)
+
+
     # Render the score and level as text
     score_text = font.render(f'Score: {point}', True, (255,255,255))
     level_text = font.render(f'Level: {level}', True, (255,255,255))
@@ -317,6 +561,7 @@ while True:
     screen.blit(paddle, paddle_rect)
     screen.blit(ball, ball_rect)
     screen.blit(ball2, ball2_rect)
+    screen.blit(ball3, ball3_rect)
 
     #ball2 = pygame.Surface((20, 20))
     screen.blit(score_text, (10,10))#Display at top left corner
@@ -327,3 +572,4 @@ while True:
     #make the paddle smaller as levels progress
     # intergrate sounds
     # load images onto rects
+    # only one of the balls - makes the game stop
